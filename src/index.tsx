d2ea5bb102d90b0f31a0d4b5fe73fbb6ac18960d
sonnylazuardi <sonnylazuardi@gmail.com>
2021-08-06 10:41:25 +0800

    Export createClient

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ export { Button } from "./Button";
 export { Battery } from "./Battery";
 export { MotionButton } from "./MotionButton";
 
-import {
-  LiveblocksProvider,
-  useOthers,
-  useMyPresence,
-  RoomProvider,
-} from "@liveblocks/react";
+// import {
+//   LiveblocksProvider,
+//   useOthers,
+//   useMyPresence,
+//   RoomProvider,
+// } from "@liveblocks/react";
 import { createClient } from "@liveblocks/client";
 
 export {
-  LiveblocksProvider,
-  useOthers,
-  useMyPresence,
-  RoomProvider,
+  //   LiveblocksProvider,
+  //   useOthers,
+  //   useMyPresence,
+  //   RoomProvider,
   createClient,
 };
