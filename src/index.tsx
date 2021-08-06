ffad89103ee7576cd239cdef0b94dbad51215d48
sonnylazuardi <sonnylazuardi@gmail.com>
2021-08-06 10:35:22 +0800

    Update build

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
 export { Button } from "./Button";
 export { Battery } from "./Battery";
 export { MotionButton } from "./MotionButton";
+
+import {
+  LiveblocksProvider,
+  useOthers,
+  useMyPresence,
+  RoomProvider,
+} from "@liveblocks/react";
+import { createClient } from "@liveblocks/client";
+
+export {
+  LiveblocksProvider,
+  useOthers,
+  useMyPresence,
+  RoomProvider,
+  createClient,
+};
