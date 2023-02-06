ad215a7c28e799cb1b721fbe15ae44e416d8c3a6
Sonny Lazuardi <sonny.lazuardi@grabtaxi.com>
2023-02-06 13:50:08 +0800

    Add 37

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ export { Battery } from "./Battery";
 export { MotionButton } from "./MotionButton";
 export { Test } from "./Test";
 
-import Map, { Marker, Layer, Source, MapRef } from "react-map-gl";
+// import Map, { Marker, Layer, Source, MapRef } from "react-map-gl";
 import { useGlobal, setGlobal, getGlobal } from "reactn";
 
-export { Map, Marker, Layer, Source, MapRef, useGlobal, setGlobal, getGlobal };
+// export { Map, Marker, Layer, Source, MapRef, useGlobal, setGlobal, getGlobal };
+export { useGlobal, setGlobal, getGlobal };
