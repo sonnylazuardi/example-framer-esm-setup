fdaa676c1e6b4d555de48159ac3501e28b0c7423
deploy <deploy@users.noreply.github.com>
2023-02-21 04:10:50 +0000

    Updated

---
+++ esmbuild@1.0.39/Button.js
@@ -0,0 +1,57 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __reExport = (target, module, desc) => {
+  if (module && typeof module === "object" || typeof module === "function") {
+    for (let key of __getOwnPropNames(module))
+      if (!__hasOwnProp.call(target, key) && key !== "default")
+        __defProp(target, key, { get: () => module[key], enumerable: !(desc = __getOwnPropDesc(module, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toModule = (module) => {
+  return __reExport(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", module && module.__esModule && "default" in module ? { get: () => module.default, enumerable: true } : { value: module, enumerable: true })), module);
+};
+
+// external-global-plugin:react
+var require_react = __commonJS({
+  "external-global-plugin:react"(exports, module) {
+    module.exports = window.React;
+  }
+});
+
+// src/Button.tsx
+var React = __toModule(require_react());
+
+// esbuild-css-modules-plugin-namespace:/tmp/tmp-1888-nCkQivECIa5M/main/src/Button.modules.css.js
+var digest = "4b54cbb1b9be55387935e56c851dbfed8fe8fef6b1f9ac6745cee5f280f24185";
+var css = `._button_kpmle_1 {
+  background-color: #c8d5ff;
+}
+`;
+(function() {
+  if (!document.getElementById(digest)) {
+    var ele = document.createElement("style");
+    ele.id = digest;
+    ele.textContent = css;
+    document.head.appendChild(ele);
+  }
+})();
+var Button_modules_css_default = { "button": "_button_kpmle_1" };
+
+// src/Button.tsx
+function Button({ title = "Title" }) {
+  return /* @__PURE__ */ React.createElement("button", {
+    className: Button_modules_css_default.button
+  }, title);
+}
+export {
+  Button
+};
---
+++ esmbuild@1.0.39/MotionButton.js
@@ -0,0 +1,46 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __reExport = (target, module, desc) => {
+  if (module && typeof module === "object" || typeof module === "function") {
+    for (let key of __getOwnPropNames(module))
+      if (!__hasOwnProp.call(target, key) && key !== "default")
+        __defProp(target, key, { get: () => module[key], enumerable: !(desc = __getOwnPropDesc(module, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toModule = (module) => {
+  return __reExport(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", module && module.__esModule && "default" in module ? { get: () => module.default, enumerable: true } : { value: module, enumerable: true })), module);
+};
+
+// external-global-plugin:react
+var require_react = __commonJS({
+  "external-global-plugin:react"(exports, module) {
+    module.exports = window.React;
+  }
+});
+
+// src/MotionButton.tsx
+var import_react = __toModule(require_react());
+import { motion } from "framer-motion";
+var style = {
+  backgroundColor: "red",
+  display: "inline-block",
+  width: 100,
+  height: 100
+};
+var MotionButton = () => /* @__PURE__ */ import_react.default.createElement(motion.button, {
+  whileHover: { scale: 0.9 },
+  whileTap: { scale: 0.7 },
+  style
+});
+export {
+  MotionButton
+};
